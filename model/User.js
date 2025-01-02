79b631bafc2a2ea7fd9a36b3ec87c87b9d879bde
Spinoff IT Solutions Pvt Limited <spinoffdigitalmarketing@gmail.com>
2025-01-02 14:29:37 +0530

    working in model setup and controller logic

--- model/User.js
+++ model/User.js
@@ -6,13 +6,22 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   mobile: { type: String, required: true },
+  profileImage: { type: String, default: null, required: false },
+  designation: { type: String, required: false }, 
+  role: { type: String, default: "user", required: false },
+  createdAt: { type: Date, default: Date.now },
 });
 
-// Encrypt password before saving
+
 UserSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
+
+UserSchema.methods.comparePassword = async function (enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
+
 
 module.exports = mongoose.model("User", UserSchema);
--- server.js
+++ server.js
@@ -5,17 +5,13 @@ const cors = require("cors");
 const authRoutes = require('./route/auth');
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB Atlas
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("MongoDB connection error:", err));
 
-
-// Routes
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
