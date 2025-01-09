ac40b1a87c12d339ebf49904d0fbe50375f89ac2
Spinoff IT Solutions Pvt Limited <141333905+spinoffTechdigital@users.noreply.github.com>
2025-01-09 12:49:56 +0530

    Update auth.js

--- route/auth.js
+++ route/auth.js
@@ -231,7 +231,7 @@ router.post("/board-of-directors", upload.single("image"), async (req, res) => {
 
 router.get("/board-of-directors", async (req, res) => {
   try {
-    const directors = await BoardOfDirector.find();
+    const directors = await Director.find();
     res.status(200).json({
       message: "Directors fetched successfully!",
       data: directors,
